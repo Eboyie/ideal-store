d7b14fb707fda7ed9a6824444675b7133e7a48eb
Francis Eboyie <80025685+Eboyie@users.noreply.github.com>
2024-11-04 16:52:20 +0200

    improve padding

--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -3,7 +3,7 @@ import TopSellers from './TopSellers';
 
 const Story = () => {
   return (
-    <div className="w-full flex justify-center flex-wrap bg-gradient-to-t from-purple-500 to-teal-500 h-screen">
+    <div className="w-full flex pb-3 justify-center flex-wrap bg-gradient-to-t from-purple-500 to-teal-500 min-h-screen">
       <TopSellers />
       <PopularBlogs />
     </div>
